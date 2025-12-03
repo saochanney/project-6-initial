133b09368c1dda7f085f34cf47c2f3673286eb55
saochanney <cha2208005@maricopa.edu>
2025-12-02 19:35:53 -0700

    updated to after class

--- dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
+++ dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
@@ -6,8 +6,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DictionaryApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DictionaryApplication.class, args);
-    }
-
 }
